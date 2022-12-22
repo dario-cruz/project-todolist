77eafb602e2cd023e51ea0a50dc604124ed4df95
Dario Cruz <rio2012@gmail.com>
2022-12-22 09:39:51 -0500

    Move most of code in newTaskModal into if statement checking for modal presence in dom.

--- src/components/page-elements.js
+++ src/components/page-elements.js
@@ -66,130 +66,135 @@ const createDefaultPageContent = () => {
 // Create modal so that the user can add a task to the project.
 // On submit allow the form to close on form submit.
 const newTaskModal = (appendElement) => {
-    // Define all elements needed to complete the modal.
-    // Modal will allow for entering of task information.
-    const modalDiv = document.createElement('div')
-    const modalContent = document.createElement('div')
-    const spanElement = document.createElement('span')
-    const modalForm = document.createElement('form')
-    // Set up all of the names and ID's for the form for submit.
-    const newTaskInput = document.createElement('input')
-    const newTaskInputLabel = document.createElement('label')
-    const newTaskDetail = document.createElement('input')
-    const newTaskDetailLabel = document.createElement('label')
-    const newTaskDate = document.createElement('input')
-    const newTaskDateLabel = document.createElement('label')
-    // Create elements to allow for the user to select priority of sev of the project.
-    const newTaskPriority = document.createElement('select')
-    const newTaskPriorityLabel = document.createElement('label')
-    // Create options for the select element.
-    const taskPriorityOne = document.createElement('option')
-    const taskPriorityTwo = document.createElement('option')
-    const taskPriorityThree = document.createElement('option')
-
-    // Create submit button to attach event to. 
-    const newTaskSubmit = document.createElement('button')
-
-    // Setup all of the attributes for the elements to be appended.
-    modalDiv.setAttribute('class', 'modal-div')
-    // Set class for closing and opening modal. 
-    modalDiv.setAttribute('class', 'closed-modal')
-    modalContent.setAttribute('class', 'modal-content')
-    spanElement.setAttribute('class', 'span-close')
-    modalForm.setAttribute('action', '')
-    modalForm.setAttribute('id', 'task-form')
-    modalDiv.setAttribute('id', 'modal-div')
+    if (document.getElementById('modal-div') == null) {
+        // Define all elements needed to complete the modal.
+        // Modal will allow for entering of task information.
+        const modalDiv = document.createElement('div')
+        const modalContent = document.createElement('div')
+        const spanElement = document.createElement('span')
+        const modalForm = document.createElement('form')
+        // Set up all of the names and ID's for the form for submit.
+        const newTaskInput = document.createElement('input')
+        const newTaskInputLabel = document.createElement('label')
+        const newTaskDetail = document.createElement('input')
+        const newTaskDetailLabel = document.createElement('label')
+        const newTaskDate = document.createElement('input')
+        const newTaskDateLabel = document.createElement('label')
+        // Create elements to allow for the user to select priority of sev of the project.
+        const newTaskPriority = document.createElement('select')
+        const newTaskPriorityLabel = document.createElement('label')
+        // Create options for the select element.
+        const taskPriorityOne = document.createElement('option')
+        const taskPriorityTwo = document.createElement('option')
+        const taskPriorityThree = document.createElement('option')
     
-    // Form attributes for linking labels.
-    newTaskInput.setAttribute('type', 'text')
-    newTaskInput.setAttribute('id', 'task-input')
-    newTaskInput.setAttribute('placeholder', '...enter task name')
-    newTaskInputLabel.setAttribute('for', 'task-input')
-
-    // Set all of the attributes for the task details.
-    newTaskDetail.setAttribute('type', 'text-area')
-    newTaskDetail.setAttribute('col', '5')
-    newTaskDetail.setAttribute('rows', '50')
-    newTaskDetail.setAttribute('placeholder', '....enter some details')
-    newTaskDetail.setAttribute('class', 'task-detail')
-    newTaskDetail.setAttribute('id', 'task-detail')
-
-    // Set attributes for task labels.
-    newTaskInputLabel.innerText = "Task Name"
-    newTaskDetailLabel.innerText = "Task Details"
-    // Give the span element and X to click and close the modal.
-    spanElement.innerText = "&times;"
-
-    // Set attributes for submit button
-    newTaskSubmit.setAttribute('class', 'modal-submit-button') 
-    newTaskSubmit.setAttribute('id', 'modal-submit-button')
-    newTaskSubmit.setAttribute('type', 'submit')
-    newTaskSubmit.innerText = "Submit"
+        // Create submit button to attach event to. 
+        const newTaskSubmit = document.createElement('button')
     
-    // Set attribute for date form
-    newTaskDate.setAttribute('type', 'date')
-    newTaskDate.setAttribute('value', `${getCurrentDate()}`)
-    newTaskDate.setAttribute('class', 'task-date')
-    newTaskDate.setAttribute('id', 'task-date')
-    newTaskDateLabel.setAttribute('for', 'task-date')
+        // Setup all of the attributes for the elements to be appended.
+        modalDiv.setAttribute('class', 'modal-div')
+        // Set class for closing and opening modal. 
+        modalDiv.setAttribute('class', 'closed-modal')
+        modalContent.setAttribute('class', 'modal-content')
+        spanElement.setAttribute('class', 'span-close')
+        modalForm.setAttribute('action', '')
+        modalForm.setAttribute('id', 'task-form')
+        modalDiv.setAttribute('id', 'modal-div')
+        
+        // Form attributes for linking labels.
+        newTaskInput.setAttribute('type', 'text')
+        newTaskInput.setAttribute('id', 'task-input')
+        newTaskInput.setAttribute('placeholder', '...enter task name')
+        newTaskInputLabel.setAttribute('for', 'task-input')
+    
+        // Set all of the attributes for the task details.
+        newTaskDetail.setAttribute('type', 'text-area')
+        newTaskDetail.setAttribute('col', '5')
+        newTaskDetail.setAttribute('rows', '50')
+        newTaskDetail.setAttribute('placeholder', '....enter some details')
+        newTaskDetail.setAttribute('class', 'task-detail')
+        newTaskDetail.setAttribute('id', 'task-detail')
+    
+        // Set attributes for task labels.
+        newTaskInputLabel.innerText = "Task Name"
+        newTaskDetailLabel.innerText = "Task Details"
+        // Give the span element and X to click and close the modal.
+        spanElement.innerText = "&times;"
+    
+        // Set attributes for submit button
+        newTaskSubmit.setAttribute('class', 'modal-submit-button') 
+        newTaskSubmit.setAttribute('id', 'modal-submit-button')
+        newTaskSubmit.setAttribute('type', 'submit')
+        newTaskSubmit.innerText = "Submit"
+        
+        // Set attribute for date form
+        newTaskDate.setAttribute('type', 'date')
+        newTaskDate.setAttribute('value', `${getCurrentDate()}`)
+        newTaskDate.setAttribute('class', 'task-date')
+        newTaskDate.setAttribute('id', 'task-date')
+        newTaskDateLabel.setAttribute('for', 'task-date')
+    
+        // Attributes for the priority select and options.
+        newTaskPriority.setAttribute('name', 'task-priority-name')
+        newTaskPriority.setAttribute('id', 'task-priority')
+        newTaskPriority.setAttribute('form', 'task-form')
+        newTaskPriorityLabel.setAttribute('for', 'task-priority-name')
+        newTaskPriorityLabel.innerText = "Select Priority"
+    
+        taskPriorityOne.setAttribute('value', '1')
+        taskPriorityTwo.setAttribute('value', '2')
+        taskPriorityThree.setAttribute('value', '3')
+    
+    
+        // Append the elements to one another.
+        modalDiv.append(modalContent)
+        modalContent.append(spanElement, modalForm)
+        modalForm.append(newTaskInputLabel, newTaskInput)
+        modalForm.append(newTaskDetailLabel, newTaskDetail)
+        modalForm.append(newTaskPriorityLabel, newTaskPriority)
+        modalForm.append(newTaskDateLabel, newTaskDate)
+        newTaskPriority.append(taskPriorityOne, taskPriorityTwo, taskPriorityThree)
+        modalForm.append(newTaskSubmit)
+        // Append entire element to argument element.
+        appendElement.append(modalDiv)
+    
+        // Create and event so that the users submitted input gets converted to a new
+        // task inside the project object. Make use of the class functions defined in
+        // the module.
+        newTaskSubmit.addEventListener('submit', (event) => {
+            event.preventDefault()
+            // Get current project that is selected and if none are selected do nothing.
+            let taskPanel = document.getElementById('task-panel')
+    
+            if (taskPanel.getAttribute('data-object') == null) {
+                return
+            } else {
+                let currentProject = taskPanel.getAttribute('data-object')    
+                let objetToMod = projectList.findIndex(prop => prop.projectName == currentProject)
+                let taskName = newTaskInput.value
+                let taskNotes = newTaskDetail.value
+                let taskDate = newTaskDate.value
+                let taskPriority = newTaskPriority.value
+                
+                // Gather all of the form data and process into a task for project.
+                projectList[objetToMod].makeNewTask(taskName, taskPriority, taskNotes, taskDate)
+            }
+        })
+    } else {
+        console.log('modal already present.')
+    }
+    
+    // Create eventlistener so that when to addTask button is pressed the modal opens so 
+    // the user can enter some information. 
+    const newTaskEvent = (targetElement, classProp) => {
+        // Need this to toggle the display of the modal element when creating a new task.
+        // Create functionality to toggle the display of element.
+        targetElement.addEventListener('click', () => {
+            targetElement.classList.toggle(`${classProp}`)
+        })
+    }
 
-    // Attributes for the priority select and options.
-    newTaskPriority.setAttribute('name', 'task-priority-name')
-    newTaskPriority.setAttribute('id', 'task-priority')
-    newTaskPriority.setAttribute('form', 'task-form')
-    newTaskPriorityLabel.setAttribute('for', 'task-priority-name')
-    newTaskPriorityLabel.innerText = "Select Priority"
 
-    taskPriorityOne.setAttribute('value', '1')
-    taskPriorityTwo.setAttribute('value', '2')
-    taskPriorityThree.setAttribute('value', '3')
-
-
-    // Append the elements to one another.
-    modalDiv.append(modalContent)
-    modalContent.append(spanElement, modalForm)
-    modalForm.append(newTaskInputLabel, newTaskInput)
-    modalForm.append(newTaskDetailLabel, newTaskDetail)
-    modalForm.append(newTaskPriorityLabel, newTaskPriority)
-    modalForm.append(newTaskDateLabel, newTaskDate)
-    newTaskPriority.append(taskPriorityOne, taskPriorityTwo, taskPriorityThree)
-    modalForm.append(newTaskSubmit)
-    // Append entire element to argument element.
-    appendElement.append(modalDiv)
-
-    // Create and event so that the users submitted input gets converted to a new
-    // task inside the project object. Make use of the class functions defined in
-    // the module.
-    newTaskSubmit.addEventListener('submit', (event) => {
-        event.preventDefault()
-        // Get current project that is selected and if none are selected do nothing.
-        let taskPanel = document.getElementById('task-panel')
-
-        if (taskPanel.getAttribute('data-object') == null) {
-            return
-        } else {
-            let currentProject = taskPanel.getAttribute('data-object')    
-            let objetToMod = projectList.findIndex(prop => prop.projectName == currentProject)
-            let taskName = newTaskInput.value
-            let taskNotes = newTaskDetail.value
-            let taskDate = newTaskDate.value
-            let taskPriority = newTaskPriority.value
-            
-            // Gather all of the form data and process into a task for project.
-            projectList[objetToMod].makeNewTask(taskName, taskPriority, taskNotes, taskDate)
-        }
-    })
-
-}
-
-// Create eventlistener so that when to addTask button is pressed the modal opens so 
-// the user can enter some information. 
-const newTaskEvent = (targetElement, classProp) => {
-    // Need this to toggle the display of the modal element when creating a new task.
-    // Create functionality to toggle the display of element.
-    targetElement.addEventListener('click', () => {
-        targetElement.classList.toggle(`${classProp}`)
-    })
 }
 
 const getCurrentDate = () => {
