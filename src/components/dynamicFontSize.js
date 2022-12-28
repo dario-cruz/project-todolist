d08a848a58e356acdec30f4979ae9c3db19a0b79
Dario Cruz <rio2012@gmail.com>
2022-12-28 08:40:20 -0500

    Add comments to dynamic text module.

--- src/components/dynamicFontSize.js
+++ src/components/dynamicFontSize.js
@@ -1,10 +1,12 @@
+// Allows for dynamic sizing of text, based off of the length of the text.
+// Used for keeping project list and task notes from overflowing out  of the containing div. 
 const dynamicFontSize = (textLength) => {
     const baseFontSize = 9
     if (textLength >= baseFontSize) {
         textLength = baseFontSize - 2
     }
     const fontSize = baseFontSize - textLength
-    return `${fontSize}vw`
+    return `${fontSize}rem`
 }
 
 export {dynamicFontSize}
