1fe3b7d1b398f44430e6da63376119a2ee260ea4
Cruz <dariocru@amazon.com>
2023-01-24 11:34:47 -0500

    Add more attrubitues for taskEditModal.

--- src/components/taskEditModal.js
+++ src/components/taskEditModal.js
@@ -13,12 +13,23 @@ const taskEditModal = (targetElement) => {
     attributeHelper(EditModalContent, {'id':'edit-modal-content'})
 
     const EditForm = document.createElement('form')
+    attributeHelper(EditForm, {'action':'', 'id':'task-edit-form'})
     
     const EditTaskName = document.createElement('input')
+    attributeHelper(EditTaskName, {'type':'text', 'id':'edit-task-name'})
 
-    const EditTaskDetail = document.createElement('input')
+    const EditTaskDetail = document.createElement('textarea')
+    attributeHelper(EditTaskDetail, {'id':'edit-task-detail', 'rows':'', 'cols':''})
 
     const EditTaskPriority = document.createElement('select')
+    attributeHelper(EditTaskPriority, {'id':'edit-task-priority', 'name':'edit-task-priority'})
+    
+    const lowPriority = document.createElement('option')
+    attributeHelper(lowPriority, {'value':'low'})
+    const medPriority = document.createElement('option')
+    attributeHelper(medPriority, {'value':'med'})
+    const hiPriority = document.createElement('option')
+    attributeHelper(hiPriority, {'value':'hi'})
 
     const EditCancel = document.createElement('button')
 
