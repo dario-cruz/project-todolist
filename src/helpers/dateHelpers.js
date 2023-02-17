023c7509b210f504ae396f908e90e86933737a02
Dario Cruz <rio2012@gmail.com>
2023-02-17 07:59:17 -0500

    Remove un-needed code from dateHelper.js.

--- src/helpers/dateHelpers.js
+++ src/helpers/dateHelpers.js
@@ -4,11 +4,6 @@ import {format} from "date-fns"
 // This will keep the started for selection pointed to the current day.
 const getCurrentDate = () => {
     let today = format(new Date(), 'MM/dd/yyyy')
-
-    // let day = String(today.getDate()).padStart(2, '0')
-    // let month = String(today.getMonth() + 1).padStart(2, '0')
-    // let year = today.getFullYear()
-    // today = day + '/' + month + '/' + year
     return today
 }
 
