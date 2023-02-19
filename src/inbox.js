9884ff2693ef1943896b8919dd32cd2fe6df3b1f
Dario Cruz <rio2012@gmail.com>
2023-02-19 13:37:21 -0500

    Add duedate to inbox place holders on page load.

--- src/inbox.js
+++ src/inbox.js
@@ -8,8 +8,8 @@ const makeDefaultProject = () => {
         // Create default project "inbox" and add default task to the project.
         const inboxProject = new Project('Inbox')
         // Create elements for the left-panel. 
-        inboxProject.makeNewTask('Clean the Kitchen', 'Hi', 'Make sure to clean under the cabinet.')
-        inboxProject.makeNewTask('Take out Trash', 'Low', 'Remember to sort the recycling.')
+        inboxProject.makeNewTask('Clean the Kitchen', 'Hi', 'Make sure to clean under the cabinet.', '2030-01-02')
+        inboxProject.makeNewTask('Take out Trash', 'Low', 'Remember to sort the recycling.', '2023-02-02')
     
         //Add to localStorage
         placeInStorage(inboxProject)
