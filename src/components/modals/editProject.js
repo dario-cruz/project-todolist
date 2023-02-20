500851a3c2c9d6580406ca67b9230ce3cef6c41e
Dario Cruz <rio2012@gmail.com>
2023-02-19 19:12:16 -0500

    Add more elements and attributes to editProject.js

--- src/components/modals/editProject.js
+++ src/components/modals/editProject.js
@@ -1,4 +1,5 @@
 import { attributeHelper } from "../../helpers/attributeHelper"
+import { currentItem, targetObject } from "../../helpers/currentItemHolder"
 
 // Modal Container
 const editProjectContainer = document.createElement('div')
@@ -14,5 +15,23 @@ attributeHelper(editProjectForm, {'id':'edit-project-form', 'action':''})
 
 // Form Inputs
 const editProjectNameLabel = document.createElement('label')
+attributeHelper(editProjectNameLabel, {'for':'edit-project-input','form':'edit-project-form'})
 const editProjectName = document.createElement('input')
-attributeHelper(editProjectName, {'type':'text', 'required':''})
+attributeHelper(editProjectName, {'id':'edit-project-input','type':'text', 'required':''})
+
+// Submit button
+const editProjectSubmit = document.createElement('button')
+attributeHelper(editProjectSubmit, {'id':'edit-project-submit'})
+
+// Span element for closing modal.
+const editProjectSpan = document.createElement('span')
+editProjectSpan.innerHTML = '&times'
+
+
+// Exportable func for adding element to the dom.
+const editProjectModal = (targetElement) => {
+
+}
+
+
+export {editProjectModal}
