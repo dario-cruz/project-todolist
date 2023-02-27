53eaae65af9c81a8e393cf0ba51162cab6429439
Cruz <dariocru@amazon.com>
2023-02-27 10:03:52 -0500

    Mod the regex code for projectValidation.js

--- src/components/Validation/projectValidation.js
+++ src/components/Validation/projectValidation.js
@@ -2,7 +2,8 @@
 
 // Define the regex pattern, filtering out numbers and special characters.
 // const projectValReg = /([A-Za-z]+( [A-Za-z]+)+)/i
-const projectValReg = /([A-Za-z]+( [A-Za-z]+)+)[^0-9]+/i
+// const projectValReg = /([A-Za-z]+( [A-Za-z]+)+)[^0-9]+/i
+const projectValReg = /[A-Za-z\s\w]/
 const checkTheString = (theString) => {
     console.log(theString.match(projectValReg))
 }
